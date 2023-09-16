e1598982712b6a292104f32fc26ab4c4ed798204
Sebastian Betancurth <33584221+Ssbas@users.noreply.github.com>
2023-09-16 11:23:04 -0300

    Ciclos7-Ejercicios-ClaseScanner-JOptionPane.java

---
+++ SebastianBetancurth/Java/Clase4/Ciclos7-Ejercicios-ClaseScanner-JOptionPane.java
@@ -0,0 +1,22 @@
+public class Ejercicio01 {
+ public static void main (String [] args) (
+ int numero, conteo = 0, suma = 0;
+ float promedio = 0;
+  numero = Integer.parseInt (JOptionPane. showInputDialog("Digite un número: "));
+    While (numero ›= 0) { //Mientras el numero no sea negativo
+      suma += numero;
+      conteo++;
+      numero = Integer.parseInt(JOptionPane.showInputDialog("Digite otro numero: "));
+    }
+    if(conteo==0) {
+      JOptionPane.showMessageDialog(null, "Errro, la division entre cero no existe");
+    }
+    else{
+      promedio = (float) suma/conteo;
+      JOptionPane.showMessageDialog(null, "El promedio es: "+promedio);
+    }
+  }
+}
+}
+}
+}
