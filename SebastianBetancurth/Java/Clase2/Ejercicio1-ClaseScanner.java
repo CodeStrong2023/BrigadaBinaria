dcdab2935662fa4823cfa81110ef8e231fe77291
Sebastian Betancurth <33584221+Ssbas@users.noreply.github.com>
2023-09-16 01:09:02 -0300

    ClaseScanner - Ejercicio

---
+++ SebastianBetancurth/Java/Clase2/Ejercicio1-ClaseScanner.java
@@ -0,0 +1,14 @@
+publio class ciclos01
+  public static void main (String [] args) (
+    Scanner entrada = new Scanner (System.in);
+    int numero, cuadrado;
+    system.out.println("Digite un numero: ");
+    numero = Integer.parseInt(entrada.nextLine())
+      while(numero >= 0) {
+      cuadrado = (int)Math.pow(numero, 2);
+      System.out.println("El numero"+numero+"Elevado al cuadrado es: "+cuadrado);
+      System.out.println("Digite otro numero:  ");
+      numero = Integer.parseInt(entrada.nextLine());
+        }
+      }
+  }
