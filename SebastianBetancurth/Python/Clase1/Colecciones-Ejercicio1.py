ebb8d4e2a2726921b678ebbbcc48e16e3dbdc777
Sebastian Betancurth <33584221+Ssbas@users.noreply.github.com>
2023-09-16 00:02:24 -0300

    Ejercicio1 - Colecciones

---
+++ SebastianBetancurth/Python/Clase1/Colecciones-Ejercicio1.py
@@ -0,0 +1,7 @@
+# Ejercicio 1: Iterar un rango de o a 10
+# Ejemplo de ejecucion: 0,3,6,9
+print('Rango de 0 a 10 con números divisibles entre 3')
+for i in range(11):
+  if 1 % 3 == 0:
+  print(i)
+
