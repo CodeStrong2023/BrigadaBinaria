cd8fa0996b13829c7eb9bc122435f6f697a6132e
Angelina Pizzolatto <132919103+angi-pizzolatto13@users.noreply.github.com>
2023-09-18 15:31:45 -0300

    Clases 3 y 4- Tipos de datos

---
+++ AngelinaPizzolatto/Javascript/Lección1/Tipos-de-datos.js
@@ -0,0 +1,17 @@
+//Tipos de datos en Javascript 
+/*
+La sintaxis de los comentarios en javascript 
+es igual a la de Java 
+*/ 
+var nombre = 'Angi'; //Tipo String Str
+console.log(nombre); 
+
+var numero = 2000 //Tipo Numérico 
+console.log(numero);
+
+var objeto = {
+    nombre : "Ampi",
+    apellido : "Martínez",
+    edad : 18 
+}   // Tipo Objeto
+console.log(objeto)
