da582d2468b6e5f8b98da198fa72f7b921c2f96a
Angelina Pizzolatto <132919103+angi-pizzolatto13@users.noreply.github.com>
2023-09-24 00:43:08 -0300

    Utilizo las palabras return y null en la Clase PasoPorReferencia

--- AngelinaPizzolatto/Java/Clase 4/src/PasoPorReferencia/PasoPorReferencia.java
+++ AngelinaPizzolatto/Java/Clase 4/src/PasoPorReferencia/PasoPorReferencia.java
@@ -9,8 +9,23 @@ public class PasoPorReferencia {
         System.out.println("persona1 = " + persona1.nombre);
         cambiarValor(persona1); 
         System.out.println("El cambio en el nombre es: " + persona1.nombre); //si agreagmos.nombre muestra el cambio de nombre
+        Persona persona = new Persona(); 
+        //Persona persona = null; Esto muestra que el valor del objeto es inválido 
+        persona = cambiarElValor(persona); 
+        System.out.println("El nuevo valor del objeto es: " + persona.nombre);
     }
     public static void cambiarValor(Persona persona2) { //parámetro por referencia
         persona2.nombre = "Martina"; //nos muestra la dirección de memoria heap
+    }
+    //PALABRAS RETURN Y NULL
+    public static Persona cambiarElValor(Persona persona2) {
+        if (persona2 == null) {
+            System.out.println("Valor de persona2 inválido: Null");
+            return null; 
+        }
+        else {
+        persona2.nombre = "Rosa"; 
+        return persona2; 
+        }
     }
 }
