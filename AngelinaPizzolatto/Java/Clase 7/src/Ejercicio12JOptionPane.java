72a55a37eddaabe7903f10edbff2161faf2ebf14
Angelina Pizzolatto <132919103+angi-pizzolatto13@users.noreply.github.com>
2023-09-28 19:57:04 -0300

    Agregamos clase 7 con ejercicios 11,12 y encapsulamiento

---
+++ AngelinaPizzolatto/Java/Clase 7/src/Ejercicio12JOptionPane.java
@@ -0,0 +1,16 @@
+import javax.swing.JOptionPane;
+/*
+ * Pedir un número y calcular su factorial 
+ * Hacerlo con las dos clases, Scanner y JOptionPane
+ */
+public class Ejercicio12JOptionPane {
+    public static void main(String[] args) {
+        int numero = Integer.parseInt(JOptionPane.showInputDialog("Ingrese un número para obtener su factorial")); 
+        int i = 1, fact = 1; 
+        while (i<=numero) {
+            fact = fact *i;
+            i++; 
+        }
+        JOptionPane.showMessageDialog(null, "El factorial del número " + numero + " es " + fact);
+    }
+}
