7d1b97654b7572ef1536e658d63dcdf7980e073c
Santiago <santiagomkastro10@gmail.com>
2023-10-25 13:05:37 -0300

    Se agrega la clase 3 de python

---
+++ SantiagoCastro/Semestre2/Python/Clase3/Ejercicio1Clase3.py
@@ -0,0 +1,10 @@
+diccionarioSeleccion = {
+    10:{"Nombre":"Lionel Andres Messi", "Edad" : 36 , "Altura" : 1.70 , "Valor de mercado" : "35 millones" , "Posicion" : "Extremo Derecho"},
+    23:{"Nombre":"Emiliano Martinez", "Edad" : 31, "Altura" : 1.95, "Valor de mercado" : "28 millones", "Posicion" : "Arquero"},
+    11:{"Nombre":"Angel Di Maria", "Edad" : 35, "Altura" : 1.80, "Valor de mercado" : "8 millones", "Posicion" : "Extremo Derecho"},
+    13:{"Nombre":"Cristian Romero", "Edad" : 25, "Altura" : 1.85, "Valor de mercado" : "60 millones", "Posicion" : "Defensor Central"},
+    20:{"Nombre":"Alexis Mac Allister", "Edad" : 24, "Altura" : 1.76, "Valor de mercado" : "65 millones", "Posicion" : "Mediocentro"},
+    19:{"Nombre":"Nicolas Otamendi", "Edad" : 35, "Altura" : 1.83, "Valor de mercado" : "2 millones", "Posicion" : "Defensor"},
+    7:{"Nombre":"Lionel Andres Messi", "Edad" : 36, "Altura" : 1.70, "Valor de mercado" : "35 millones", "Posicion" : "Delantero"}
+}
+print (diccionarioSeleccion)
