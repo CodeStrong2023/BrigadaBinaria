142cc4abb5df49b5874d8befaf45c6d3ffe83aca
Mica Roux <135773259+MicaRoux@users.noreply.github.com>
2023-10-30 15:20:01 -0300

    Add Ejercicio1Scanner.java

---
+++ MicaRoux/Java/Clase 1-2/src/Actividades/Ejercicio1Scanner.java
@@ -0,0 +1,24 @@
+package Actividades; 
+import java.util.Scanner;
+
+public class Ejercicio1Scanner {
+    /*
+    Ejercicio 1: Leer un número y mostrar su cuadrado,
+    repeitr el proceso hasta que se introduzca un número negativo
+    */
+    public static void main(String[] args) {
+        Scanner numeros = new Scanner(System.in);  
+        int num; 
+        do {
+            System.out.println("Ingrese un número para calcular su cuadrado: ");
+            num = numeros.nextInt(); 
+            if (num>0){
+            var resultado = (int)Math.pow(num,2); 
+            System.out.println("El resultado de " + num + " elevado al cuadrado es " + resultado);}
+            else {
+                System.out.println("El número ingresado es negativo");
+            }
+        } while (num >0); 
+        numeros.close(); 
+    }
+}
