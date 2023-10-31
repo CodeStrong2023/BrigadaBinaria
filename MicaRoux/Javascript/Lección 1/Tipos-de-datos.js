f79d656d9fc371e94557789f51cefed6b100c88a
Mica Roux <135773259+MicaRoux@users.noreply.github.com>
2023-10-31 17:35:20 -0300

    se carga las lecciones de Javascript

---
+++ MicaRoux/Javascript/Lección 1/Tipos-de-datos.js
@@ -0,0 +1,17 @@
+//Tipos de datos en Javascript 
+/*
+La sintaxis de los comentarios en javascript 
+es igual a la de Java 
+*/ 
+var nombre = 'Mica'; //Tipo String Str
+console.log(nombre); 
+
+var numero = 2000 //Tipo Numérico 
+console.log(numero);
+
+var objeto = {
+    nombre : "Leo",
+    apellido : "Gutierrez",
+    edad : 18 
+}   // Tipo Objeto
+console.log(objeto)
---
+++ MicaRoux/Javascript/Lección 2/ConcatenacionDeCadenas.js
@@ -0,0 +1,80 @@
+var nombre = "Micaela"; 
+var apellido = "Roux"; 
+var nombreCompleto = nombre+ " "+ apellido; 
+console.log(nombreCompleto);
+var nombreCompleto2 = "José"+""+"Guillén"; 
+console.log("El resultado es: ", nombreCompleto2)
+var juntos = nombre + 219; //Lee de izquierda a derecha siguiendo la cadena lee el número como str
+console.log(juntos); 
+juntos = nombre + 78 + 235; //si queremos sumarlo se deben colocar paréntesis
+console.log(juntos);
+juntos = 34 + 456 +nombre; //primero suma y luego concatena
+console.log(juntos);
+
+nombre += apellido; //Concatenación con el operador simplificado
+console.log(nombre);
+
+//Hoy en día ya no se utiliza var, se utilizan len y const
+let nombre2 = "Cande"; 
+console.log(nombre2);
+
+const apellido2 = "Rodriguez";  //El apellido es una constante
+console.log(apellido2); 
+
+let x,y; //Se pueden crear varias variables dentro de una misma línea 
+x = 17, y = 23; //Se pueden realizar varias asignaciones dentro de una misma línea
+let z = x + y; 
+console.log(z); 
+
+//No se pueden definir variables con un número al inicio, ni con una palabra reservada
+// len 24mono = 23; 
+// len break = mono; 
+
+//Ampliando el uso de var, let y const
+/*Con var puede reasignar en cualquier momento,
+este forma parte del ámbito global 
+Un error es que se sobreescriba  
+*/
+
+var nombre3 = "Ezequiel"; 
+nombre3 = "Leonardo"; 
+console.log(nombre3); 
+
+function saludar() {
+    var nombre3 = "Eliana"; 
+    console.log(nombre3); 
+}
+console.log(nombre3); //Acá no lee el dato en la función
+
+if(true){
+    var edad = 46; 
+    console.log(edad); 
+}
+console.log(edad); //En la función anda correctamente, en la estructura if falla
+
+/*
+let: esta puede ser reasignada en cualquier momento
+la diferencia es que su ámbito es de bloque,
+solo disponible dentro de un bloque de llaves
+o dentro de una función 
+*/
+function saludar(){
+    let nombre4 = "Alejandra"; 
+    console.log(nombre4)
+}
+//console.log(nombre4); 
+saludar()
+if (true){
+    let edad2 = 33; 
+    console.log("Edad:", edad2 );
+}
+//console.log(edad2); 
+
+/*
+const se utiliza para valores constantes que no pueden ser reasignadas
+*/
+
+const nacimiento = 2005; 
+console.log(nacimiento); 
+//nacimiento = 2003; 
+console.log(nacimiento); //solo se ejecuta el console anterior, el segundo NO ya que es una constante y no se puede cambiar
