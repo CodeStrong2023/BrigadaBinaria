34e1ed6f960105dee8a7373d692ccc48fdf58dc8
Santino Nuñez <santinunez130@gmail.com>
2023-11-01 17:30:13 -0300

    Clase 10 Sobrecarga constructores y métodos, paquetes y palabra final

---
+++ SantinoNuñez/Java/Clase10/domain/Persona.java
@@ -0,0 +1,8 @@
+package domain;
+
+public class Persona {
+    public final static int CONSTANTE_AQUI = 32; // Es una constante y no se puede modificar
+    public void imprimir() {
+        System.out.println("Hola, soy una persona");
+    }
+}
