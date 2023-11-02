8d394102485d2aa63073ab1070d79769acdc5f0d
Santino Nuñez <santinunez130@gmail.com>
2023-11-02 12:23:34 -0300

    Clase 9 JavaScript Operadores y Ejercicios

--- SantinoNuñez/JavaScript/Clase_3/03-01-TiposDeDatos.js
+++ SantinoNuñez/JavaScript/Clase_3/03-01-TiposDeDatos.js
@@ -25,3 +25,43 @@ if(dentroRango>= valMin && dentroRango<=valMax){
 else{
     console.log("El valor NO está dentro del rango")
 }
+
+// Ejercicio si el padre puede asistir al juego del hijo
+let vacaciones = false;
+let diaLibre = false;
+if(vacaciones || diaLibre){     // el operador or es ||
+    console.log("Puede asistir al partido")
+}
+else{
+    console.log("No puede asistir al partido")
+}
+
+// Operador Ternario
+let resultado2 = 3 > 6 ? "Verdadero":"Falso";
+console.log(resultado2)
+let numero = 37;
+resultado2 = numero %2 == 0 ? "Es número Par":"Es número Impar";
+console.log(resultado2)
+
+// Convertir String en Number
+let miNumero = "23";   // numero cadena
+console.log(typeof miNumero)
+let edad2 = Number(miNumero)     // Esta es una funcion 
+console.log(typeof edad2)
+
+// Función isNan
+if(isNaN(edad2)){          // isNan compara si solo hay números. Is not a number (devuelve booleano)
+    console.log("La variable no contiene solo numeros")
+}
+else{
+    if(edad2 >= 18){
+        console.log("Puede votar a Milei")
+    }
+    else{
+        console.log("No puedes votar")
+    }
+}
+
+//Ternario
+let resultado3 = edad2>= 18 ? "Puede votar":"No puede votar"
+console.log(resultado3)
