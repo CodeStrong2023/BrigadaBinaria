0a303b85368e941d625439f5771fe26d0fbea614
Mica Roux <135773259+MicaRoux@users.noreply.github.com>
2023-11-02 19:36:19 -0300

    Delete MicaRoux directory

--- MicaRoux/Java/Palabra Final/src/domain/Empleado.java
+++
@@ -1,8 +0,0 @@
-package domain;
-
-public class Empleado extends Persona {
-    @Override
-    public void imprimir() { //no se puede sobreescribir porque es un método constante
-        System.out.println("Soy un empleado");
-    }
-}
