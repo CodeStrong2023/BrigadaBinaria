89e6686879550c52d5e8151d181509d207022c99
Santiago <santiagomkastro10@gmail.com>
2023-11-03 23:54:07 -0300

    Se agrega la clase 7 de JS

---
+++ SantiagoCastro/Semestre2/JavaScript/Clase7/EjerciciosC7/Ejercicio1C7.js
@@ -0,0 +1,10 @@
+//Ejercicio 1: Encontrar numeros pares e impares
+
+var numero = 10;
+
+if(numero % 2 == 0){
+    console.log("El numero es PAR")
+}
+else{
+    console.log("El numero es IMPAR")
+}
