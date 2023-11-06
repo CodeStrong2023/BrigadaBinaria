2b1bcc6176ba9ee6f07fd1251f6c6ae896b74565
Gianella Nahuel <136371589+gianellanahuel@users.noreply.github.com>
2023-11-06 10:56:39 -0300

    Update Ejercicio07Scanner.java

--- GianellaNahuel/Java/Clase 4/Ejercicios/Ejercicio07Scanner.java
+++ GianellaNahuel/Java/Clase 4/Ejercicios/Ejercicio07Scanner.java
@@ -1,1 +1,26 @@
+import java.util.Scanner;
+
+public class Ejercicio07Scanner {
+/* Ejercicio 07: Pedir numeros hasta que se introduzca uno negativo
+y calcular la media
+*/
+    public static void main(String[] args) {
+        Scanner numeros = new Scanner(System.in);
+        int num, contador = 0; 
+        double suma = 0, media = 0; 
+        System.out.println("Digite un número");
+        num = numeros.nextInt();  
+        while (num >0) {
+            contador ++; 
+            suma += num;
+            media = (suma/contador); 
+            System.out.println("Digite otro número");
+            num = numeros.nextInt();  
+        }
+        System.out.println("La media es: " + media);
+        numeros.close(); 
+    }
+}
+
+
 
