45feafa6f99a57794aa9b15a1d443c7aad103c1a
Gianella Nahuel <136371589+gianellanahuel@users.noreply.github.com>
2023-11-05 23:51:03 -0300

    Update Ejercicio2Scanner.java

--- GianellaNahuel/Java/Clase 1-2/Actividades/Ejercicio2Scanner.java
+++ GianellaNahuel/Java/Clase 1-2/Actividades/Ejercicio2Scanner.java
@@ -1,1 +1,31 @@
+package Actividades; 
+import java.util.Scanner;
+
+public class Ejercicio2Scanner {
+    /*
+    Ejercicio 2: Leer un número e indicar si es positivo o negativo.
+    El proceso se repetirá hasta que se introduzca un 0 
+    */
+    public static void main(String[] args) {
+        Scanner numeros = new Scanner(System.in);
+        int num; 
+        System.out.println("Ingrese un número sin coma");
+        num = numeros.nextInt(); 
+        while (num != 0) {
+            if (num<0) {
+                System.out.println("El número ingresado es negativo");
+            } 
+            else if (num>0) {
+                System.out.println("El número ingresado es positivo");
+            }
+            else {
+                System.out.println("El número ingresado es 0");
+                break; 
+            }
+            System.out.println("Ingrese otro número");
+            num = numeros.nextInt(); 
+        }
+        numeros.close(); 
+    }
+}
 
