88dcadebe1c2a5736b018b19b6ead325a0e4a893
Gianella Nahuel <136371589+gianellanahuel@users.noreply.github.com>
2023-11-06 10:53:42 -0300

    Update Ejercicio07OptionPane.java

--- GianellaNahuel/Java/Clase 4/Ejercicios/Ejercicio07OptionPane.java
+++ GianellaNahuel/Java/Clase 4/Ejercicios/Ejercicio07OptionPane.java
@@ -1,1 +1,22 @@
+import javax.swing.JOptionPane;
+
+public class Ejercicio07OptionPane {
+/* Ejercicio 07: Pedir numeros hasta que se introduzca uno negativo
+y calcular la media
+*/
+    public static void main(String[] args) {
+        int num, contador = 0; 
+        double suma = 0, media = 0; 
+        num = Integer.parseInt(JOptionPane.showInputDialog(null, "Digite un numero")); 
+        while (num >0) {
+            contador ++; 
+            suma += num;
+            media = (suma/contador); 
+            num = Integer.parseInt(JOptionPane.showInputDialog(null, "Digite otro numero"));  
+        }
+        JOptionPane.showMessageDialog(null, "La media es: " + media);
+    }
+}
+
+
 
