b5139a7b281e867603d27c04417209c6546261a8
Gianella Nahuel <136371589+gianellanahuel@users.noreply.github.com>
2023-11-06 16:39:57 -0300

    Update Aritmetica.java

--- GianellaNahuel/Java/Clase 5/src/Operaciones/Aritmetica.java
+++ GianellaNahuel/Java/Clase 5/src/Operaciones/Aritmetica.java
@@ -1,1 +1,29 @@
+package operaciones; 
 
+public class Aritmetica { 
+    int a; 
+    int b; 
+    public Aritmetica() { 
+        System.out.println("Se está ejecutando el constructor número 1");
+    }
+    public Aritmetica(int a, int b) {  
+        this.a = a; 
+        this.b = b; 
+        System.out.println("Se está ejecutando el constructor número 2");
+    }
+    
+    public void sumarNumeros() { 
+        int resultado = (a + b); 
+        System.out.println("El resultado es " + resultado);
+    }
+    public int sumarConRetorno() {
+        int resultado = a+b; 
+        return resultado; 
+    }
+    public int sumarConArgumentos(int arg1, int arg2) {
+        a = arg1; 
+        b = arg2; 
+        
+        return this.sumarConRetorno(); 
+    }
+}
