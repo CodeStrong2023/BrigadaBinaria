eebdca0b7b1d6e5cc364a039993a39d9d0ee3176
Gianella Nahuel <136371589+gianellanahuel@users.noreply.github.com>
2023-11-06 00:38:50 -0300

    Update Ejercicio04OptionPane.java

--- GianellaNahuel/Java/Clase 3/src/Ejercicio04OptionPane.java
+++ GianellaNahuel/Java/Clase 3/src/Ejercicio04OptionPane.java
@@ -1,1 +1,21 @@
 
+import javax.swing.JOptionPane;
+
+public class Ejercicio04OptionPane {
+/*
+Ejercicio: Pedir números hasta que se teclee uno negativo 
+y mostrar cuantos números se han introducido
+Con la clase Scanner
+ */
+    public static void main(String[] args) {
+        int num;
+        num = Integer.parseInt(JOptionPane.showInputDialog("Digite un número "));  
+        int contador = 0; 
+        while (num > 0) {
+            contador++;
+            num = Integer.parseInt(JOptionPane.showInputDialog("Digite otro número ")); 
+        }
+        int cant_num = contador + 1; 
+        System.out.println("La cantidad de números ingresados fue de: "+ (cant_num)); 
+    }
+}
