c5b7ef61f6e7f709a2170af4aa521fdb5ef4d542
Gianella Nahuel <136371589+gianellanahuel@users.noreply.github.com>
2023-11-05 23:49:24 -0300

    Update Ejercicio2OptionPane.java

--- GianellaNahuel/Java/Clase 1-2/Actividades/Ejercicio2OptionPane.java
+++ GianellaNahuel/Java/Clase 1-2/Actividades/Ejercicio2OptionPane.java
@@ -1,1 +1,24 @@
+package Actividades; 
+import javax.swing.JOptionPane;
+
+public class Ejercicio2OptionPane {
+    public static void main(String[] args) {
+        //mismo ejercicio sin clase Scanner 
+        int num; 
+        num = Integer.parseInt(JOptionPane.showInputDialog("Ingrese un número sin coma")); 
+        while (num != 0) {
+            if (num<0) {
+                System.out.println("El número ingresado es negativo");
+            } 
+            else if (num>0) {
+                System.out.println("El número ingresado es positivo");
+            }
+            else {
+                System.out.println("El número ingresado es 0");
+                break; 
+            }
+            num = Integer.parseInt(JOptionPane.showInputDialog("Ingrese otro número ")); 
+        }
+    }
+}
 
