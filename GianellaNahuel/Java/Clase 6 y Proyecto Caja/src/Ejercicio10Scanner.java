a5ca6c6f974a67980f3fb20702222cb51620b5e6
Gianella Nahuel <136371589+gianellanahuel@users.noreply.github.com>
2023-11-06 20:50:32 -0300

    Update Ejercicio10Scanner.java

--- GianellaNahuel/Java/Clase 6 y Proyecto Caja/src/Ejercicio10Scanner.java
+++ GianellaNahuel/Java/Clase 6 y Proyecto Caja/src/Ejercicio10Scanner.java
@@ -1,1 +1,19 @@
+import java.util.Scanner;
 
+public class Ejercicio10 {
+    //Ejercicio 10: Pedir 10 números y escribir la suma total 
+    //Hacerlo con la clase Scanner y JOptionPane
+    public static void main(String[] args) throws Exception {
+        Scanner numeros = new Scanner(System.in);
+        int i, num, acumulador;
+        acumulador = 0; 
+        System.out.println("Digite 10 núemeros");
+        for (i=1;i<=10;i++) {
+            System.out.println("Digite un número");
+            numero  = numeros.nextInt(); 
+            suma= suma + num; 
+        }
+        System.out.println("La suma de todos los números ingresados es de: " + suma);
+        numeros.close(); 
+    }
+}
