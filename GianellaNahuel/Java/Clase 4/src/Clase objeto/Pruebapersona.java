1e05c167c88c05ff292fc16cccb28e78bf4b5bfc
Gianella Nahuel <136371589+gianellanahuel@users.noreply.github.com>
2023-11-06 11:13:25 -0300

    Update Pruebapersona.java

--- GianellaNahuel/Java/Clase 4/src/Clase objeto/Pruebapersona.java
+++ GianellaNahuel/Java/Clase 4/src/Clase objeto/Pruebapersona.java
@@ -1,1 +1,15 @@
-
+package clases;
+public class PruebaPersona {
+    public static void main(String[] args) {
+        Persona persona1; 
+        persona1 = new Persona(); //llamamos al constructor 
+        persona1.nombre = "Gianella"; 
+        persona1.apellido = "Nahuel"; 
+        persona1.obtenerInformacion(); 
+      
+        Persona persona2 = new Persona(); 
+        persona2.nombre = "agustina"; 
+        persona2.apellido = "Martinez"; 
+        persona2.obtenerInformacion();
+    }
+}
