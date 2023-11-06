76d532fa63cd95a91ec64d1e6de8580c5e9b54bd
Gianella Nahuel <136371589+gianellanahuel@users.noreply.github.com>
2023-11-05 23:13:14 -0300

    Update While.java

--- GianellaNahuel/Java/Clase1-2/src/While.java
+++ GianellaNahuel/Java/Clase1-2/src/While.java
@@ -1,1 +1,9 @@
-
+public class While {
+    public static void main(String[] args)  {
+        var conteo = 0; 
+        while (conteo <= 10){
+            System.out.println("Conteo = " + conteo); 
+            conteo++; //Se va aumentando la variable 
+        }
+    }
+}
