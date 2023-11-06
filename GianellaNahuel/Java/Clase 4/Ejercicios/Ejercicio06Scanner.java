865885f5e72a896b81f201d4a34a89271c6e32d0
Gianella Nahuel <136371589+gianellanahuel@users.noreply.github.com>
2023-11-06 10:59:48 -0300

    Update Ejercicio06Scanner.java

--- GianellaNahuel/Java/Clase 4/Ejercicios/Ejercicio06Scanner.java
+++ GianellaNahuel/Java/Clase 4/Ejercicios/Ejercicio06Scanner.java
@@ -1,1 +1,21 @@
+import java.util.Scanner;
 
+public class Ejercicio06Scanner {
+/* Ejercicio 06: Pedir numeros hasta que se teclee 0, 
+mostrar la suma de todos los números introducidos
+ */
+    public static void main(String[] args) {
+        Scanner numeros = new Scanner(System.in);
+        int numero, suma; 
+        suma = 0; 
+        System.out.println("Digite un número");
+        numero = numeros.nextInt();  
+        while (numero != 0){
+            suma = suma + numero;
+            System.out.println("Digite otro número");
+            numero = numeros.nextInt();  
+        }
+        System.out.println("La suma de todos los números es de: " + suma);
+        numeros.close(); 
+    }
+}
