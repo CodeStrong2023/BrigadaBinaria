12f2401f4f599cc923fecfaee66bff3018c23117
Gianella Nahuel <136371589+gianellanahuel@users.noreply.github.com>
2023-11-05 23:18:52 -0300

    Delete GianellaNahuel/Java/Clase1-2 directory

--- GianellaNahuel/Java/Clase1-2/src/DoWhile.java
+++
@@ -1,9 +0,0 @@
-public class DoWhile {
-    public static void main(String[] args) {
-        var contador = 0; 
-        do {
-            System.out.println("Contador = " + contador);
-            contador++; 
-        } while(contador <= 7); 
-    }
-}
