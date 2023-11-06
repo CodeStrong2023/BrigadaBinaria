12a6a455ff92a4400f052b5c6cc6187787b551d8
Gianella Nahuel <136371589+gianellanahuel@users.noreply.github.com>
2023-11-06 20:09:10 -0300

    Update Caja.java

--- GianellaNahuel/Java/Clase 6 y Proyecto Caja/Proyecto caja/Caja.java
+++ GianellaNahuel/Java/Clase 6 y Proyecto Caja/Proyecto caja/Caja.java
@@ -1,1 +1,20 @@
+package caja;
 
+public class Caja { //Clase Publica caja 
+    //Atributos(Caracteristicas)
+    int ancho; 
+    int alto; 
+    int profundo; 
+    //Métodos y constructores(Acciones)
+    public Caja() { //constructor 1=vacío
+    }
+    public Caja(int ancho, int alto, int profundidad) {//constructor 2 
+        this.ancho = ancho; 
+        this.alto = alto; 
+        this.profundo = profundidad; 
+    }
+    public int calcularVolumen() { //Método para calcular el volumen 
+        return volumen = ancho * alto * profundo; 
+    
+    }
+}
