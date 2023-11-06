0f3cc53333e42dd5e5854460a180da59f81538c9
Gianella Nahuel <136371589+gianellanahuel@users.noreply.github.com>
2023-11-06 17:23:02 -0300

    Update Ejercicio08OptionPane.java

--- GianellaNahuel/Java/Clase 5/src/Ejercicio08OptionPane.java
+++ GianellaNahuel/Java/Clase 5/src/Ejercicio08OptionPane.java
@@ -1,1 +1,15 @@
+package Ejercicios;
+//Ejercicio 08: pedir un número N, y mostrar todos los números del 1 al N
 
+import javax.swing.*;
+
+public class Ejercicio08JOptionPane{
+    public static void main(String[] args) {
+        int numero = Integer.parseInt(JOptionPane.showInputDialog("Digite un número: "));
+        int i = 1;
+        while (i<=numero) {
+            JOptionPane.showMessageDialog(null,i);
+            i++;
+        }
+    }
+}
