f5b58ac6ac731e70290a7d96628fac4570426713
Gianella Nahuel <136371589+gianellanahuel@users.noreply.github.com>
2023-11-06 10:49:46 -0300

    Update Ejercicio06OptionPane.java

--- GianellaNahuel/Java/Clase 4/Ejercicios/Ejercicio06OptionPane.java
+++ GianellaNahuel/Java/Clase 4/Ejercicios/Ejercicio06OptionPane.java
@@ -1,1 +1,17 @@
+import javax.swing.JOptionPane;
 
+public class Ejercicio06OptionPane {
+/* Ejercicio 06: Pedir números hasta que se teclee 0, 
+mostrar la suma de todos los números introducidos
+ */
+    public static void main(String[] args) {
+        int numero, suma; 
+        suma = 0; 
+        numero = Integer.parseInt(JOptionPane.showInputDialog("Ingrese un numero"));
+        while (numero != 0){
+            suma = suma + numero;
+            numero = Integer.parseInt(JOptionPane.showInputDialog("Ingrese otro numero")); 
+        }
+        JOptionPane.showMessageDialog(null, "La suma de todos los numeros es de: " + suma);
+    }
+}
