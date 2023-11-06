6af4f2c04c96dcc413631671535c23bbd8361930
Gianella Nahuel <136371589+gianellanahuel@users.noreply.github.com>
2023-11-06 16:57:23 -0300

    Update Ejercicio08Scanner.java

--- GianellaNahuel/Java/Clase 5/src/Ejercicio08Scanner.java
+++ GianellaNahuel/Java/Clase 5/src/Ejercicio08Scanner.java
@@ -1,1 +1,18 @@
+package Ejercicios;
+//Ejercicio 08: pedir un número N, y mostrar todos los números del 1 al N
 
+import java.util.Scanner;
+
+public class Ejercicio08 {
+    public static void main(String[] args) {
+        Scanner entrada = new Scanner(System.in);
+        System.out.println("Digite un número: ");
+        int numero = Integer.parseInt(entrada.nextLine());
+
+        int i = 1;
+        while (i<=numero) {
+            System.out.println(i);
+            i++;
+        }
+    }
+}
