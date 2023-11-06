7873d3509435cacd0979f0e045b149a0ede5a2b7
Gianella Nahuel <136371589+gianellanahuel@users.noreply.github.com>
2023-11-06 20:44:28 -0300

    Update Ejercicio10OptionPane.java

--- GianellaNahuel/Java/Clase 6 y Proyecto Caja/src/Ejercicio10OptionPane.java
+++ GianellaNahuel/Java/Clase 6 y Proyecto Caja/src/Ejercicio10OptionPane.java
@@ -1,1 +1,16 @@
+import javax.swing.JOptionPane;
 
+public class Ejercicio10{
+        //Ejercicio 10: Pedir 10 números y escribir la suma total 
+    //Hacerlo con la clase Scanner y JOptionPane
+    public static void main(String[] args) throws Exception {
+        int i, num, suma;
+        acumulador = 0; 
+        JOptionPane.showMessageDialog(null,"Digite 10 números");
+        for (i=1;i<=10;i++) {
+           num = Integer.parseInt(JOptionPane.showInputDialog(null, "Digite un número: ")); 
+            acumulador= acumulador + num; 
+        }
+        JOptionPane.showMessageDialog(null, "La suma de todos los números es: " + acumulador);
+    }
+}
