29dd8affbf07abe77d0fb39074b01a0c87b4f010
Gianella Nahuel <136371589+gianellanahuel@users.noreply.github.com>
2023-11-06 11:17:51 -0300

    Update Persona.java

--- GianellaNahuel/Java/Clase 4/src/Clase objeto/Persona.java
+++ GianellaNahuel/Java/Clase 4/src/Clase objeto/Persona.java
@@ -1,1 +1,13 @@
-
+package clases;
+public class Persona {
+        //Atributos de la clase (características)
+        public String nombre; 
+        public String apellido; 
+  
+        //Metodos de la clase (Acciones) 
+        public void obtenerInformacion() {
+            System.out.println("Nombre: " + nombre);
+            System.out.println("Apellido: " + apellido);
+          
+    }
+}
