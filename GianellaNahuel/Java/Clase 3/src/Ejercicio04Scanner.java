01dd711f7dde00ee1b1dd6a4808d266ca20fd13d
Gianella Nahuel <136371589+gianellanahuel@users.noreply.github.com>
2023-11-06 00:43:45 -0300

    Update Ejercicio04Scanner.java

--- GianellaNahuel/Java/Clase 3/src/Ejercicio04Scanner.java
+++ GianellaNahuel/Java/Clase 3/src/Ejercicio04Scanner.java
@@ -1,1 +1,24 @@
+import java.util.Scanner;
 
+public class Ejercicio04Scanner {
+/*
+Ejercicio: Pedir números hasta que se teclee uno negativo 
+y mostrar cuantos números se han introducido
+Con la clase Scanner
+ */
+    public static void main(String[] args) {
+        Scanner numeros = new Scanner(System.in);
+        int num;
+        System.out.println("Digite un número");
+        num = numeros.nextInt();  
+        int contador = 0; 
+        while (num > 0) {
+            contador++;
+            System.out.println("Digite otro número");
+            num = numeros.nextInt();
+        }
+        int cant_num = contador + 1; 
+        System.out.println("La cantidad de números ingresados fue de: "+ (cant_num));
+        numeros.close(); 
+    }
+}
