6e9f6a7377fb81e4ca9d006858a6e03079ff8887
Gianella Nahuel <136371589+gianellanahuel@users.noreply.github.com>
2023-11-06 00:34:43 -0300

    Update Ejercicio03Scanner.java

--- GianellaNahuel/Java/Clase 3/src/Ejercicio03Scanner.java
+++ GianellaNahuel/Java/Clase 3/src/Ejercicio03Scanner.java
@@ -1,1 +1,26 @@
+import java.util.Scanner;
 
+public class Ejercicio03Scanner {
+/*
+Leer números hasta que se introduzca un cero
+Para cada uno indicar si es par o impar
+Con clase Scanner      
+*/
+    public static void main(String[] args)  {
+        Scanner numeros = new Scanner(System.in); 
+        System.out.println("Digite  un número");
+        int numero; 
+        numero = numeros.nextInt(); 
+        while (numero != 0){
+            if (numero % 2 == 0) {
+                System.out.println("El número ingresado es par");
+            }
+            else {
+                System.out.println("El número ingresado es impar");
+            }
+            System.out.println("Digite otro número");
+            numero = numeros.nextInt();
+        }
+        numeros.close(); 
+    }
+}
