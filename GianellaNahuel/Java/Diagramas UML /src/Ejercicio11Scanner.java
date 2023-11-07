03249d6bd4fc317eac0221e805daa776efe91cec
Gianella Nahuel <136371589+gianellanahuel@users.noreply.github.com>
2023-11-06 23:00:32 -0300

    Update Ejercicio11Scanner.java

--- GianellaNahuel/Java/Diagramas UML /src/Ejercicio11Scanner.java
+++ GianellaNahuel/Java/Diagramas UML /src/Ejercicio11Scanner.java
@@ -1,1 +1,17 @@
+package Ejercicios;
+
+import javax.swing.*;
+
+/* Ejercicio 11: Diseñar un programa que muestre el producto de los
+10 primeros números impares. hacerlo con JOptionPane
+ */
+public class Ejercicio11 {
+    public static void main(String[] args) {
+        var producto = 1;
+        for (int i = 1; i<=20; i+=2){
+            producto *= i;
+        }
+        JOptionPane.showMessageDialog(null,"El producto de los primeros números impares es "+ producto);
+    }
+}
 
