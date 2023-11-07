96cbc25e53a77d714991cbd135f3aea1c2efda08
Gianella Nahuel <136371589+gianellanahuel@users.noreply.github.com>
2023-11-07 18:57:55 -0300

    Create Ejercicio01.js

---
+++ GianellaNahuel/Javascript/Clase 8/Ejercicio01.js
@@ -0,0 +1,11 @@
+//Ejercicio 1: Dentro de un rango
+
+let dentroRango = 5;
+let valMin = 0 , valMax = 10;
+
+if (dentroRango >= valMin && dentroRango <= valMax){
+    console.log("Dentro del rango establecido")
+}
+else{
+    console.log("Fuera del rango establecido")
+}
