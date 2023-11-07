d948afb098721debfa8e804c7bd34e1e81175e76
Gianella Nahuel <136371589+gianellanahuel@users.noreply.github.com>
2023-11-06 23:04:26 -0300

    Update Ejercicio12Scanner.java

--- GianellaNahuel/Java/Diagramas UML /src/Ejercicio12Scanner.java
+++ GianellaNahuel/Java/Diagramas UML /src/Ejercicio12Scanner.java
@@ -1,1 +1,20 @@
+import java.util.Scanner;
+/*
+ * Pedir un número y calcular su factorial 
+ * Hacerlo con las dos clases, Scanner y JOptionPane
+ */
+public class Ejercicio12Scanner {
+    public static void main(String[] args) {
+        Scanner factorial = new Scanner(System.in);
+        System.out.println("Digite un número entero: ");
+        int numero = factorial.nextInt(); 
+        int i = 1, fact = 1; 
+        while (i<=numero) {
+            fact = fact *i;
+            i++; 
+        }
+        System.out.println("El factorial de " + numero + " es " + fact);
+        factorial.close(); 
+    }
+}
 
