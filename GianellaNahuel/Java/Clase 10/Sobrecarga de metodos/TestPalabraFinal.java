77bcf64f3c336d499792fb9397fc2554f21ecd98
Gianella Nahuel <136371589+gianellanahuel@users.noreply.github.com>
2023-11-07 17:37:23 -0300

    Update TestPalabraFinal.java

--- GianellaNahuel/Java/Clase 10/Sobrecarga de metodos/TestPalabraFinal.java
+++ GianellaNahuel/Java/Clase 10/Sobrecarga de metodos/TestPalabraFinal.java
@@ -1,1 +1,13 @@
+package test;
+// import ar.com.codesystem
+// import ar.com.codesystem.Utileria;
+
+public class TestUtileria {
+    public static void main(String[] args) {
+    //    Utileria.imprimir("saludos a todos");
+    //    imprimir("saludos a todos");
+        ar.com.codesystem.Utileria.imprimir("llamamos al paquete a la clase ");
+    }
+}
+
 
