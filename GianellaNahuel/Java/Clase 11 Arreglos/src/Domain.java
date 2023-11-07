5dddb7d32e3a1814b9175f78721d8114ddcb0b39
Gianella Nahuel <136371589+gianellanahuel@users.noreply.github.com>
2023-11-07 17:47:56 -0300

    Update Domain.java

--- GianellaNahuel/Java/Clase 11 Arreglos/src/Domain.java
+++ GianellaNahuel/Java/Clase 11 Arreglos/src/Domain.java
@@ -1,1 +1,24 @@
+package domain;
+
+public class Persona {
+    private String nombre;
+
+    public Persona(String nombre) {
+        this.nombre = nombre;
+    }
+
+    public String getNombre() {
+        return nombre;
+    }
+
+    public Persona setNombre(String nombre) {
+        this.nombre = nombre;
+    }
+
+    @Override
+    public String toString() {
+        return “persona(“ + “Nombre =” +nombre +  ´ }´+”,super.toString ( ) ;
+
+    }
+}
 
