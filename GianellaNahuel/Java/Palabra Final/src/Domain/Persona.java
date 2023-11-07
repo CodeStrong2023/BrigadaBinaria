682408fad37c76697d0846c8ea2505ca876b4d70
Gianella Nahuel <136371589+gianellanahuel@users.noreply.github.com>
2023-11-06 23:34:46 -0300

    Update Persona.java

--- GianellaNahuel/Java/Palabra Final/src/Domain/Persona.java
+++ GianellaNahuel/Java/Palabra Final/src/Domain/Persona.java
@@ -1,1 +1,49 @@
+package domain;
 
+public class Persona {
+    // Atributos
+    private int idPersona;
+    private static int contadorPersona;
+    private String nombre;
+
+    // Constructor
+    public Persona(String nombre){
+        this.nombre = nombre;
+        this.idPersona = Persona.contadorPersona;
+    }
+
+    public int getIdPersona() {
+        return idPersona;
+    }
+
+    public Persona setIdPersona(int idPersona) {
+        this.idPersona = idPersona;
+        return this;
+    }
+
+    public static int getContadorPersona() {
+        return contadorPersona;
+    }
+
+    public static void setContadorPersona(int contadorPersona) {
+        Persona.contadorPersona = contadorPersona;
+    }
+
+    public String getNombre() {
+        return nombre;
+    }
+
+    public Persona setNombre(String nombre) {
+        this.nombre = nombre;
+        return this;
+    }
+
+    @Override
+    public String toString() {
+        final StringBuilder sb = new StringBuilder("Persona{");
+        sb.append("idPersona=").append(idPersona);
+        sb.append(", nombre=").append(nombre).append('\'');
+        sb.append('}');
+        return sb.toString();
+    }
+}
