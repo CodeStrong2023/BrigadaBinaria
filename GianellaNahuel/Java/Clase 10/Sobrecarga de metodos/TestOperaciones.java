151635948ee8cb1baf634107d9d217d3d48830a9
Gianella Nahuel <136371589+gianellanahuel@users.noreply.github.com>
2023-11-07 17:36:59 -0300

    Update TestOperaciones.java

--- GianellaNahuel/Java/Clase 10/Sobrecarga de metodos/TestOperaciones.java
+++ GianellaNahuel/Java/Clase 10/Sobrecarga de metodos/TestOperaciones.java
@@ -1,1 +1,18 @@
+package test;
+
+import Operaciones.Operaciones;
+
+public class TestOperaciones {
+    public static void main(String[] args) {
+        var resultado = Operaciones.sumar(54, 87);
+        System.out.println("resultado = " + resultado);
+
+        var resultado2 = Operaciones.sumar(23.4,52.1); // si es entero llama al metodo int (enteros)
+        System.out.println("resultado2 = " + resultado2);
+
+        var resultado3 = Operaciones.sumar(8,9l); // metodo int no soporta long, por eso llama al metodo double
+        System.out.println("resultado3 = " + resultado3);
+    }
+}
+
 
