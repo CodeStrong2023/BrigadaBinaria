31a5fc0e01a28b9136a88d45af602d8f6b04adaa
Gianella Nahuel <136371589+gianellanahuel@users.noreply.github.com>
2023-11-07 17:48:30 -0300

    Update Test.java

--- GianellaNahuel/Java/Clase 11 Arreglos/src/Test.java
+++ GianellaNahuel/Java/Clase 11 Arreglos/src/Test.java
@@ -1,1 +1,17 @@
+package test;
+
+import domain.Persona;
+
+public class TestArreglosObject {
+    public static void main(String[] args) {
+        Persona personas[] = new Persona[2];
+        personas[0] = new Persona("gianella");
+        personas[1] = new Persona("Luciana");
+        System.out.println("persona 0 = " + personas[0]);
+        System.out.println("persona 1 = " + personas[1]);
+
+        for(int i = 0; i < personas.length; i++){
+            System.out.println("personas "+i+ " = " + personas[i]);
+    }
+}
 
