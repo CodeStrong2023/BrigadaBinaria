58067ad37655b1db0332dff783d20b9019bc39be
Gianella Nahuel <136371589+gianellanahuel@users.noreply.github.com>
2023-11-07 17:49:36 -0300

    Update Ejercicio con arreglo1.java

--- GianellaNahuel/Java/Clase 11 Arreglos/Ejercicio con arreglos/Ejercicio con arreglo1.java
+++ GianellaNahuel/Java/Clase 11 Arreglos/Ejercicio con arreglos/Ejercicio con arreglo1.java
@@ -1,1 +1,20 @@
+package Ejercicios_Arreglo;
+
+import java.util.Scanner;
+
+// Ejercicio1: leer 5 numeros , guardarlos en un arreglo,y mostrarlos en el mismo orden introducidos
+public class Ejercicio1 {
+    public static void main(String[] args) {
+        Scanner entrada = new Scanner(System.in);
+        int Arreglo[] = new int[5];
+        
+        for (int i = 0; i < 5 ; i++) {
+            System.out.println((i+1)+"Digite un numero: ");
+            Arreglo[i] = entrada.nextInt();
+        }
+        for (int i :Arreglo) {
+            System.out.println(i+ " ");
+        }
+    }
+}
 
