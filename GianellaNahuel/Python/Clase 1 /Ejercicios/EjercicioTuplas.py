99fdad88fa413b853f9bec442ad865570d5bf8f5
Gianella Nahuel <136371589+gianellanahuel@users.noreply.github.com>
2023-11-08 00:24:41 -0300

    Create EjercicioTuplas.py

---
+++ GianellaNahuel/Python/Clase 1 /Ejercicios/EjercicioTuplas.py
@@ -0,0 +1,7 @@
+# Dada la siguiente tupla 
+tupla = (13,1,8,3,2,5,8)
+#Crear una lista que solo incluya los números menores a 5 e imprima por consola [1,3,2]
+for elemento in tupla: 
+    if elemento<5: 
+        lista = (elemento)
+        print(lista)
