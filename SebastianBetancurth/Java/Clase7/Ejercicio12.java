772be8df367bbb6cab9d491ec79a57f94b83a859
Sebastian Betancurth <33584221+Ssbas@users.noreply.github.com>
2023-11-08 00:37:54 -0300

    Clase7 -  Ejercicio12 - Java

---
+++ SebastianBetancurth/Java/Clase7/Ejercicio12.java
@@ -0,0 +1,16 @@
+/*Ejercicio 12: Pedir un número y calcular su factorial. Hacerlo con ambas clases, Scanner y JOptionPane*/
+package ejercicio12;
+import java.util.Scanner;
+public class Ejercicio12 {
+    public static void main(String[] args) {
+        Scanner entrada = new Scanner(System.in);
+        long F = 1;
+        System.out.println("Ingrese un valor: ");
+        int N = Integer.parseInt(entrada.next());
+        
+        for (int I = 1; I <= N; I++){
+            F *= I;
+        }
+        System.out.println("El factorial de " + N + " es: " + F);
+    }   
+}
