028a36261970c4fdf58f863beea588297223dc1e
Gianella Nahuel <136371589+gianellanahuel@users.noreply.github.com>
2023-11-08 01:18:29 -0300

    Create Persona9.py

---
+++ GianellaNahuel/Python/Clase 8 y 9/Persona9.py
@@ -0,0 +1,13 @@
+# Los atributos son caracteristicas
+# los metodos son el comportamiento que van a tener los objetos (acciones)
+persona1.mostrar_detalle()     
+ # persona2.mostrar_detalle()    
+# Persona.mostrar_detalle(persona1) 
+persona1.nombre = "Franco"
+print(f"El nombre es : {persona1.nombre} {persona1.nombre}") 
+# print(persona2.nombre) 
+persona4 = Persona("Luna","Perez", 22, )
+persona5 = Persona("sol","Gutierrez", 20,)
+persona4.mostrar_detalle()
+persona5.mostrar_detalle()
+# persona4.__nombre #  Está encapsulado
