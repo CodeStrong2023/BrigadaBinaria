30262d4d76784a0da80c6c4694e6bf99af7ee4e0
Gianella Nahuel <136371589+gianellanahuel@users.noreply.github.com>
2023-11-08 00:19:55 -0300

    Create tuplas.py

---
+++ GianellaNahuel/Python/Clase 1 /tuplas.py
@@ -0,0 +1,12 @@
+Cocina = ("cuchara","cuchillo","Tenedor","plato")
+print(Cocina)
+
+cant_elementos = len(Cocina)
+print(f" cantidad de elementos es {cant_elementos}")
+print(Cocina[2])
+print(Ccocina[-1])
+
+print(Ccocina[0:2])
+#elementos de una tupla 
+for elemento in Cocina: 
+    print(elemento, end=' ') #el end se usa para eliminar saltos de línea
