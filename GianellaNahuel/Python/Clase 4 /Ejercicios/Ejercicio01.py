04126cf6076d54142e4a2ead5092923021e21ce4
Gianella Nahuel <136371589+gianellanahuel@users.noreply.github.com>
2023-11-08 00:35:56 -0300

    Create Ejercicio01.py

---
+++ GianellaNahuel/Python/Clase 4 /Ejercicios/Ejercicio01.py
@@ -0,0 +1,9 @@
+''' Ejercicio 1: Eliminar duplicado de una lista 
+Escriba un programa donde tenga una lista y que a continuación 
+elimine los elementos repetidos, por último mostrar la lista 
+'''
+//Creamos una lista 
+print("EJERCICIO 1")
+lista = [1,2,3,'Giane ', 7,7,3,'Daniel',”Giane”,1, “Daniel”,2,]
+print("LISTA“)
+lista = list(set) 
