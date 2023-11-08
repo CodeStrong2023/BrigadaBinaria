44712ba7923a39c40019446e91ef8471b7499173
Sebastian Betancurth <33584221+Ssbas@users.noreply.github.com>
2023-11-08 00:55:42 -0300

    Clase 10 Operaciones.java

---
+++ SebastianBetancurth/Java/Clase10/SobreCargoDeMetodos/Operaciones.java
@@ -0,0 +1,11 @@
+package Operaciones;
+public class Operaciones {
+     public static int Sumar(int A, int B){
+        System.out.println("Método para sumar números enteros");
+        return A + B;
+    }
+    public static double Sumar(double A, double B){
+        System.out.println("Metodo para sumar double");
+        return A + B;
+    }
+}
