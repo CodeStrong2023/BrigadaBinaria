2ed8f45724c324fc34016bf95719e2753c804662
Sebastian Betancurth <33584221+Ssbas@users.noreply.github.com>
2023-11-08 02:00:49 -0300

    Leccion 2 ReutilizarVariables - JavaScript

---
+++ SebastianBetancurth/Python/JavaScript/Leccion2/ReutilizarVariables.js
@@ -0,0 +1,20 @@
+var Nombre = "Sebastian"; 
+console.log(typeof Nombre);
+
+Nombre = 7;
+console.log(typeof Nombre);
+
+Nombre = 12.3;
+console.log(typeof Nombre);
+
+var Numero = 3000 
+console.log(Numero);
+
+var Objeto = {
+    Nombre: "Sebastian",
+    Apellido: "Betancurth",
+    Telefono: 2604648581
+}
+
+console.log(Objeto);
+console.log(typeof Objeto);
