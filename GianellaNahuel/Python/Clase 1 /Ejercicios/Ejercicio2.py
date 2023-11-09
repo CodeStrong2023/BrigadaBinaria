c2d97aca6215e004ea26c48d9dca7f08b856e0b9
Santino Nuñez <santinunez130@gmail.com>
2023-11-09 19:19:30 -0300

    Prueba de reparacion de archivos

--- GianellaNahuel/Python/Clase 1 /Ejercicios/Ejercicio2.py
+++
@@ -1,4 +0,0 @@
-print("Ejercicio 2") #Crear un rango de números de 2 a 6 e imprimelos 
-#Ejemplo de ejecución: 2,3,4,5,6
-for i in range(2,7): 
-    print(i)
--- GianellaNahuel/Python/Clase 4 /Ejercicio_Matematica.py
+++
@@ -1,7 +0,0 @@
-# Ejercicio de matemáticas 
-# Para sacar la raiz cuadrada de un número positivo
-numero = int(input("Digite un número: "))
-while numero < 0:
-    print("Error-> debería ser un número positivo")
-    numero = int(input("Digite un número positivo: "))
-print(f"\n La raiz cuadrada de {numero} es: {math.sqrt(numero):.2f}")
